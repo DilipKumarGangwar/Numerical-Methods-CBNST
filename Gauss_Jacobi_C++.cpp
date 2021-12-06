1bd0081f790adc549be1ff2f090a6b1dfd725740
DilipKumarGangwar <gangwardilipkumar@gmail.com>
2021-12-06 16:20:11 +0530

    commit

--- Gauss_Jacobi_C++.cpp
+++ Gauss_Jacobi_C++.cpp
@@ -1,14 +1,14 @@
 //C Program to Implement Gauss Jacobi Method
-#include<stdio.h>
-#include<math.h>
-#include<stdbool.h>
-#define EPSILON 0.001
+#include<iostream>
+#include<cmath>
+using namespace std;
 
-int n;
+#define EPSILON 0.001  // till 3 correct decimal places
+
 int flag;
 
 //Helper function
-float findSum(int i,float a[][n+1])
+float findSum(int i,float a[50][51],int n)
 {
      float sum=0;
      for(int j=0;j<n;j++)
@@ -20,14 +20,14 @@ float findSum(int i,float a[][n+1])
 } 
 
 //checks if Gauss Jacobi Method is applicable and return true if yes otherwise return false
-bool isMethodApplicable(float a[][n+1]) 
+bool isMethodApplicable(float a[50][51],int n)
 {
    for(int i=0;i<n;i++)
    {
        for(int j=0;j<n;j++)
        {
 
-           if(fabs(a[i][i])>findSum(i,a))
+           if(fabs(a[i][i])>findSum(i,a,n))
               break;
            else  
               return false;   
@@ -38,16 +38,16 @@ bool isMethodApplicable(float a[][n+1])
 }
 
 //prints the Value of Unknowns
-void print(int iteration,float values[n]) 
+void print(int iteration,float values[],int n) 
 {
-  printf("Iteration %d ",iteration);  
+  cout<<"Iteration "<<iteration<< ": ";  
   for(int i=0;i<n;i++)
-    printf("value[%d]=%f ",i+1,values[i]);
-  printf("\n");
+    cout<<"value["<<i+1<<"]= "<<values[i]<<" ";
+  cout<<endl;
 }
 
 
-void findValues(float a[][n+1],int maxIterations,float values_old[n])
+void findValues(float a[50][51],int maxIterations,float values_old[],int n)
 {
     int i,j,k,iteration;
     float sum=0;
@@ -70,10 +70,7 @@ void findValues(float a[][n+1],int maxIterations,float values_old[n])
         for(k=0;k<n;k++)
         {
             if(fabs(values_old[k]-values_new[k])<EPSILON)
-            {   
                 continue;
-                
-            }
             else
             {
                flag=1;
@@ -83,50 +80,50 @@ void findValues(float a[][n+1],int maxIterations,float values_old[n])
         } 
         if(flag==0)
         {
-            print(iteration,values_new);  // print final values of unknowns  and return
+            print(iteration,values_new,n);  // print final values of unknowns  and return
             return ;
         }
         flag=0; //resetting the flag
        
-       print(iteration,values_new); //To print intermediate values of unknowns
+       print(iteration,values_new,n); //To print intermediate values of unknowns
 
         //copy new values of unknowns to old value array
         for(k=0;k<n;k++)
              values_old[k]=values_new[k];
 
     } //end of iteration loop
-    print(iteration,values_new) ; 
+    print(iteration,values_new,n) ; 
 
 } //end of findValues()
 
  
 int main()
 {
-    int i,j,k,x,y,maxIterations;
+    int i,j,k,x,y,maxIterations,n;
     float ratio;
-    printf("Enter no of Unknowns\n");
-    scanf("%d",&n);
-    printf("Enter no. of iterations\n");
-    scanf("%d",&maxIterations);
-    float a[n][n+1];
-    float values[n];
+    cout<<"Enter no of Unknowns\n";
+    cin>>n;
+    cout<<"Enter no. of iterations\n";
+    cin>>maxIterations;
+    float a[50][51];
+    float values[50];;
 
-    printf("Enter the Augmented Matrix\n");
+    cout<<"Enter the Augmented Matrix\n";
     for(int i=0;i<n;i++)
     {
      for(int j=0;j<n+1;j++)
-        scanf("%f",&a[i][j]);
+        cin>>a[i][j];
     }
 
-   if(!isMethodApplicable(a))
+   if(!isMethodApplicable(a,n))
    {
-        printf("Gauss Jacobi Method can't be applied\n");
+        cout<<"\nGauss Jacobi Method can't be applied";
         return 0;
    }
-   printf("Gauss Jacobi Method is applicable\n");
+   cout<<"\nGauss Jacobi Method is applicable\n\n";
    for(int i=0;i<n;i++)
      values[i]=0;
-   findValues(a,maxIterations,values);
+   findValues(a,maxIterations,values,n);
    return 0;
 }
 
--- Gauss_Seidel_C++.cpp
+++ Gauss_Seidel_C++.cpp
@@ -1,15 +1,13 @@
 //C Program to Implement Gauss Seidel Method
 
-#include<stdio.h>
-#include<math.h>
-#include<stdbool.h>
+#include<iostream>
+#include<cmath>
+using namespace std;
 #define EPSILON 0.001  // till 3 correct decimal places
 
-int n;
-int flag;
 
 //Helper function
-float findSum(int i,float a[][n+1])
+float findSum(int i,float a[50][51],int n)
 {
      float sum=0;
      for(int j=0;j<n;j++)
@@ -21,15 +19,15 @@ float findSum(int i,float a[][n+1])
 }
 
 //checks if Gauss Seidel Method is applicable and return true if yes otherwise return false
-bool isMethodApplicable(float a[][n+1])
+bool isMethodApplicable(float a[50][51],int n)
 {
    for(int i=0;i<n;i++)
    {
        for(int j=0;j<n;j++)
        {
 
-           if(fabs(a[i][i])>findSum(i,a))
-              continue;
+           if(fabs(a[i][i])>findSum(i,a,n))
+              break;
            else  
               return false;   
 
@@ -39,22 +37,24 @@ bool isMethodApplicable(float a[][n+1])
 }
 
 //prints the Value of Unknowns
-void print(int iteration,float values[n]) 
+void print(int iteration,float values[],int n) 
 {
-  printf("Iteration %d ",iteration);  
+  cout<<"Iteration "<<iteration<< ": ";  
   for(int i=0;i<n;i++)
-    printf("value[%d]=%f ",i+1,values[i]);
-  printf("\n");
+    cout<<"value["<<i+1<<"]= "<<values[i]<<" ";
+  cout<<endl;
 }
 
 
-void findValues(float a[][n+1],int maxIterations,float values_old[n])
+void findValues(float a[50][51],int maxIterations,float values_old[],int n)
 {
-    int i,j,k,iteration;
+    int i,j,k,iteration,flag=0;
     float ratio,sum=0;
     float values_new[n];
+    //This loop is added for Gauss seidel ( not present in Jacobi method)
     for(int i=0;i<n;i++)
      values_new[i]=0;
+
     for(iteration=1;iteration<=maxIterations;iteration++)
     {
         for(i=0;i<n;i++)
@@ -63,14 +63,13 @@ void findValues(float a[][n+1],int maxIterations,float values_old[n])
             for(j=0;j<n;j++)
             {
                 if(i!=j)
-                sum+=a[i][j]*values_new[j];
+                sum+=a[i][j]*values_new[j];   // in Gauss Jacobi, we took values_old[] here
             }
-          
+           
             values_new[i]=(a[i][n] - sum)/a[i][i];
         }
-        
         //Now you have found the values of n unknowns for above iteration
-        //Now check if your matching criteria satisfied , comparing with previous iteration values
+        //Now check if your matching criteria satisfied , comparing with previous iteration values 
         for(k=0;k<n;k++)
         {
             if(fabs(values_old[k]-values_new[k])<EPSILON)
@@ -83,54 +82,51 @@ void findValues(float a[][n+1],int maxIterations,float values_old[n])
         } 
         if(flag==0)
         {
-            print(iteration,values_new);  // print final values of unknowns  and return
+            print(iteration,values_new,n);  // print final values of unknowns  and return 
             return ;
         }
         flag=0; //resetting the flag
        
-        print(iteration,values_new); //To print intermediate roots
+        print(iteration,values_new,n); //To print intermediate roots
 
         //copy new values of unknowns to old value array
         for(k=0;k<n;k++)
              values_old[k]=values_new[k];
     } //end of iteration loop
-    print(iteration,values_new) ; 
+    print(iteration,values_new,n) ; 
 
 } //end of findValues()
 
  
 int main()
 {
-    int i,j,k,x,y,maxIterations;
+    int i,j,k,x,y,maxIterations,n;
     float ratio;
-    printf("Enter no of Unknowns\n");
-    scanf("%d",&n);
-    printf("Enter no. of iterations\n");
-    scanf("%d",&maxIterations);
-    float a[n][n+1];
-    float values[n];;
+    cout<<"Enter no of Unknowns\n";
+    cin>>n;
+    cout<<"Enter no. of iterations\n";
+    cin>>maxIterations;
+    float a[50][51];
+    float values[50];;
 
-    printf("Enter the Augmented Matrix\n");
+    cout<<"Enter the Augmented Matrix\n";
     for(int i=0;i<n;i++)
     {
      for(int j=0;j<n+1;j++)
-        scanf("%f",&a[i][j]);
+        cin>>a[i][j];
     }
 
-   if(!isMethodApplicable(a))
+   if(!isMethodApplicable(a,n))
    {
-        printf("Gauss Seidel Method can't be applied");
+        cout<<"\nGauss Seidel Method can't be applied";
         return 0;
    }
-   printf("Gauss Seidel Method is applicable");
+   cout<<"\nGauss Seidel Method is applicable\n\n";
    for(int i=0;i<n;i++)
      values[i]=0;
-   findValues(a,maxIterations,values);
+   findValues(a,maxIterations,values,n);
    return 0;
 }
-
-
-
 
 
 
